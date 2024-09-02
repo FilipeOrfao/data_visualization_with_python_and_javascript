5790c37d76f6243adf357bc8d8d03e42b8602361
filipeorfao <filipeaorfao@gmail.com>
2024-09-02 18:47:12 +0100

    fix extract_country and no death dates and place

--- 022_/nobel_laureates/nobel_laureates/spiders/laureates_spiders.py
+++ 022_/nobel_laureates/nobel_laureates/spiders/laureates_spiders.py
@@ -13,20 +13,20 @@ class ListSpider(scrapy.Spider):
 
         countries = response.xpath("//h3")
 
+        # for l in response.css("ol>li:has(>a)")[:10]:
         for l in response.css("ol>li:has(>a)"):
-            # for l in response.css("ol>li:has(>a)")[:10]:
             nwinner = NWinnerItem()
 
             nwinner["name"] = l.css("a::text").get()
-            # nwinner["country"] = l.xpath("preceding::h3[1]").xpath("text()").get()
-            # try:
-            #     nwinner["category"] = l.get().split(",")[-2].strip()
-            # except:
-            #     nwinner["category"] = None
-            # try:
-            #     nwinner["year"] = int(re.findall("\d{4}", l.get().split(",")[-1])[0])
-            # except:
-            #     nwinner["year"] = None
+            nwinner["country"] = l.xpath("preceding::h3[1]").xpath("text()").get()
+            try:
+                nwinner["category"] = l.get().split(",")[-2].strip()
+            except:
+                nwinner["category"] = None
+            try:
+                nwinner["year"] = int(re.findall("\d{4}", l.get().split(",")[-1])[0])
+            except:
+                nwinner["year"] = None
             nwinner["link"] = f'{l.css("a::attr(href)").get()}'
 
             # yield nwinner
@@ -56,50 +56,49 @@ class ListSpider(scrapy.Spider):
             item["date_of_birth"] = None
 
         # getting date of death
-        # try:
-        #     death_info = response.xpath("//tr[contains(., 'Died')]/td")[0].get()
-        #     item["date_of_death"] = extract_date(death_info)
-        #     item["date_of_death"] = fix_date(item["date_of_death"])
+        try:
+            death_info = response.xpath("//tr[contains(., 'Died')]/td")[0].get()
+            item["date_of_death"] = extract_date(death_info)
+            item["date_of_death"] = fix_date(item["date_of_death"])
 
-        # except IndexError:
-        #     item["date_of_death"] = None
+        except IndexError:
+            item["date_of_death"] = None
 
         # fix this because it does not work for everyone use their position in the list
         try:
-            item["place_of_birth"] = extract_birth_country(birth_info)
+            item["place_of_birth"] = extract_country(birth_info)
         except IndexError as e:
             print(e)
             item["place_of_birth"] = None
 
-        # try:
-        #     item["place_of_death"] = (
-        #         response.xpath(
-        #             "//span[@class='bday']/parent::*/parent::*/parent::*/following-sibling::*[1]//text()"
-        #         )
-        #         .getall()[-1]
-        #         .split(",")[-1]
-        #         .strip()
-        #     )
-        # except IndexError:
-        #     item["place_of_death"] = None
+        try:
+            if death_info:
+                item["place_of_death"] = extract_country(death_info)
+            else:
+                item["place_of_death"] = None
+        except UnboundLocalError as e:
+            print(e)
+            item["place_of_death"] = None
 
-        # try:
-        #     item["profile_pic"] = (
-        #         f'https:{"/".join(response.css(".mw-file-element::attr(src)").get().split("/")[:-1])}'.replace(
-        #             "/thumb", ""
-        #         )
-        #     )
-        # except:
-        #     item["profile_pic"] = None
+        try:
+            item["profile_pic"] = (
+                f'https:{"/".join(response.css(".mw-file-element::attr(src)").get().split("/")[:-1])}'.replace(
+                    "/thumb", ""
+                )
+            )
+        except:
+            item["profile_pic"] = None
 
-        # try:
-        #     item["award_age"] = item["year"] - int(item["date_of_birth"][-4:])
-        # except:
-        #     item["award_age"] = None
+        try:
+            item["award_age"] = int(item["year"]) - int(
+                datetime.fromtimestamp(item["date_of_birth"]).strftime("%Y")
+            )
+        except:
+            item["award_age"] = None
 
-        # item["text"] = response.xpath(
-        #     "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2]"
-        # ).extract()[0]
+        item["text"] = response.xpath(
+            "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2]"
+        ).extract()[0]
 
         yield item
 
@@ -123,10 +122,10 @@ def fix_date(date):
     return formatted_date
 
 
-def extract_birth_country(birth_info):
-    birth_country = re.findall(r"(>,*\s*\w+\s*\w*\s*\w*\s*\w*<|U\.S\.|US)", birth_info)[
-        -1
-    ]
+def extract_country(birth_info):
+    birth_country = re.findall(
+        r"(>*,*\s*\w+\s*\w*\s*\w*\s*\w*<|U\.S\.|US)", birth_info
+    )[-1]
     birth_country = (
         birth_country.replace("<", "").replace(">", "").replace(",", "").strip()
     )
